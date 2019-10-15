67d027ea146e22c8ef5a9e27a203848f8b2df6a5
Dominic Chu <dominic.m.chu@gmail.com>
2019-10-15 14:16:45 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -13,24 +13,50 @@
 #     }
 #   }
 # }
-# 
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
 # }
+new_hoh = [
+  {
+   label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class",
+       sub_category: {
+         label: "Order",
+         sub_category: {
+           label: "Family",
+           sub_category: {
+             label: "Genus",
+             sub_category: {
+               label: "Species",
+               sub_category: {}
+             }
+           }
+         }
+       }
+     }
+   }
+ }
+]
+
 
 def naming_system
+  new_method_hoh
   # Remember:
   #  Kingdom
   #  Phylum
